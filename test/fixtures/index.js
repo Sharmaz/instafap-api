79ce6788bc6bc1b8dc24fa02953641a011e462c9
Sharmaz <irae45@gmail.com>
2016-11-22 13:02:24 -0600

    Creamos el endpoint POST /picture/:id/like hacemos el test, agregamos las propiedades likes y liked en fixtures, creamos la función likeImage en stub

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -4,6 +4,8 @@ export default {
       id: '8fc60e7c-3b3c-48e9-a6a7-a5fe4f1fbc31',
       publicId: '2fNwVYePN8WqqDFvVf7XMN',
       userId: 'instafap',
+      liked: false,
+      likes: 0,
       src: 'http://instafap.test/2fNwVYePN8WqqDFvVf7XMN.jpg',
       description: '#awesome',
       tags: ['awesome'],
